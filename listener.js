d378ac35357489b2b00d0a2a77b076791e290598
Kidon Son <kidonson@gmail.com>
2017-05-01 22:16:06 -0400

    Added incognito detection.

--- listener.js
+++ listener.js
@@ -1,5 +1,8 @@
 function newTab(callback) {
-  alert("hi");
+  // Only works if "incognito" is set to "split" in the manifest.
+  if (chrome.extension.inIncognitoContext) {
+    alert("hi");
+  }
 }
 
 chrome.tabs.onCreated.addListener(newTab);
