d12677a0903a176be22853cd32e615bd77f232ec
Kidon Son <kidonson@gmail.com>
2017-05-04 21:24:44 -0400

    Implemented 'First tab only' option

--- js/background.js
+++ js/background.js
@@ -1,10 +1,13 @@
 const NEWTAB_URL = "chrome://newtab/";
 var url;
+var firstOnly;
 
 function newTab(callback) {
   if (callback.incognito && callback.url == NEWTAB_URL) {
     getUrl(function () {
-      chrome.tabs.update(callback.id, {"url": url});
+      if (!firstOnly || callback.index == 0) {
+        chrome.tabs.update(callback.id, {"url": url});
+      }
     });
   }
 }
@@ -24,6 +27,7 @@ function loadUrl(func) {
   chrome.storage.local.get(function(result) {
     if (chrome.extension.lastError == undefined) {
       url = result["url"];
+      firstOnly = result["first_only"];
       if (func != undefined) {
         func(url);
       }
