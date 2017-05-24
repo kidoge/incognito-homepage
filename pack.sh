4a9d0aa5d20cfef822e7fac3dfdb50c0ac841c6a
Kidon Son <kidonson@gmail.com>
2017-05-24 08:18:39 -0400

    Updated pack script with regex filter and repo checks

--- pack.sh
+++ pack.sh
@@ -1,17 +1,47 @@
 #/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PACK_IGNORE="pack_ignore"
+
 is_pack_target() {
-	if [[ "$1" =~ xcf$ ]]; then
-		echo "$1 omitted"
-		return 0
+	while read -r line || [[ -n "$line" ]]; do
+		line=$(echo $line) # remove \r on Windows sytems
+		if [[ "$1" =~ $line ]]; then
+			echo "$1 omitted"
+			return 0
+		fi
+	done < $PACK_IGNORE
+
+	if [[ "$1" =~ ^store_assets ]]; then
+		return 1
 	fi
-	return 1
+}
+
+current_branch() {
+	git rev-parse --abbrev-ref HEAD
+}
+
+check_repository() {
+	if [[ "$(current_branch)" != "master" ]]; then
+		echo "ERROR: Not on master branch"
+		exit 1
+	fi
+
+	git diff-files --quiet
+	if [[ $1 -ne "0" ]]; then
+		echo "ERROR: Repository is dirty"
+		exit 1
+	fi
 }
 
 main() {
+	pushd $DIR > /dev/null
+
+	check_repository
+
 	rm -rf package
 	mkdir package
-	for f in $(git ls-tree -r master --name-only); do
+	for f in $(git ls-tree -r HEAD --name-only); do
 		if is_pack_target $f; then
 			cp --parents $f package
 		fi
@@ -20,6 +50,7 @@ main() {
 
 	#clean up
 	rm -rf package
+	popd > /dev/null
 }
 
 main "$@"
